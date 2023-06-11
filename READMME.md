c172a1aaf3b17054b995ba6f9ca6208545a542e8
HayaUp <hayaup.git@gmail.com>
2023-06-11 12:37:50 +0900

    データベースファイルを作成する。

--- READMME.md
+++ READMME.md
@@ -27,3 +27,16 @@
   - https://elf-mission.net/programming/dot-net/sqlite-nuget-packages/
 - System.Data.SQLite との比較
   - https://learn.microsoft.com/ja-jp/dotnet/standard/data/sqlite/compare
+- Microsoft.Data.Sqlite 概要
+  - https://learn.microsoft.com/ja-jp/dotnet/standard/data/sqlite/?tabs=netcore-cli
+
+### データベースファイルを作成する
+
+#### 参考
+
+- 【C#】SQLiteで今すぐデータベースを使う方法とサンプルを紹介
+  - https://marunaka-blog.com/use-sqlite-sample/1025/
+- C#でSQLite3を使ってみる
+  - https://qiita.com/koshian2/items/63938474001c510d0b15
+- 【すぐに使える】C#でSQLiteを扱うための解説とサンプル集
+  - https://resanaplaza.com/2020/09/21/%E3%80%90%E3%81%99%E3%81%90%E3%81%AB%E4%BD%BF%E3%81%88%E3%82%8B%E3%80%91c%E3%81%A7sqlite%E3%82%92%E6%89%B1%E3%81%86%E3%81%9F%E3%82%81%E3%81%AE%E5%85%B7%E4%BD%93%E4%BE%8B/
--- TrySQLiteConsole/Program.cs
+++ TrySQLiteConsole/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data.SQLite;
+using System.IO;
 
 namespace TrySQLiteConsole
 {
@@ -6,7 +8,32 @@ namespace TrySQLiteConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var sample_sqlite = new SampleSQLite();
+            sample_sqlite.CreateDatabase("sample_database.db");
+
+            Console.ReadLine();
+        }
+
+        public class SampleSQLite
+        {
+            /// <summary>
+            /// データベースファイルを作成する
+            /// </summary>
+            /// <param name="database_path">データベースファイルのパス、ファイル名のみでも可</param>
+            /// <returns>true = 作成した, false = 作成済み</returns>
+            public bool CreateDatabase(string database_path)
+            {
+                // 既にデータベースファイルを作成してある場合、テーブルやレコードが消えてしまう
+                if(File.Exists(database_path))
+                {
+                    return false;
+                }
+
+                // データベースファイル名だけの場合、作成場所は Debug または Release ディレクトリ内になる
+                SQLiteConnection.CreateFile(database_path);
+
+                return File.Exists(database_path);
+            }
         }
     }
 }
