fe7b17cbc147ccdc58aa55cdef0c506c2ee9744c
HayaUp <hayaup.git@gmail.com>
2023-06-19 01:06:49 +0900

    User クラスから不要な処理を削除。

--- TrySQLiteConsole/User.cs
+++ TrySQLiteConsole/User.cs
@@ -8,42 +8,8 @@ namespace TrySQLiteConsole
 {
     public class User
     {
-        public static readonly string TABLE_NAME = "User";
-
-        public static string SelectSQL
-        {
-            get => @$"
-                SELECT
-                    {nameof(Id)},
-                    {nameof(Name)}
-                FROM
-                    {TABLE_NAME}
-                ;";
-        }
-
         public int Id;
         public string Name;
-
-        public string CreateTableSQL
-        {
-            get => @"
-                CREATE TABLE IF NOT EXISTS User
-                (
-                    Id INTEGER NOT NULL,
-                    Name TEXT NOT NULL
-                );";
-        }
-
-        public string InsertSQL
-        {
-            get => @$"
-                INSERT INTO User (Id, Name)
-                VALUES
-                (
-                    {Id},
-                    '{Name}'
-                );";
-        }
 
         public void Show()
         {
