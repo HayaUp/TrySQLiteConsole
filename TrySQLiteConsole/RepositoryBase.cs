d2799785da91dc92261c440b52ddf8feb871dc46
HayaUp <hayaup.git@gmail.com>
2023-06-19 01:00:35 +0900

    コードを整理した。

---
+++ TrySQLiteConsole/RepositoryBase.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.IO;
+using System.Diagnostics;
+
+namespace TrySQLiteConsole
+{
+    /// <summary>
+    /// SQLite の基底クラス
+    /// </summary>
+    public class RepositoryBase
+    {
+        public readonly string DATABASE_NAME;
+
+        public RepositoryBase()
+        {
+#warning データベース名は決め打ちでよさそう
+            DATABASE_NAME = "sample_database.db";
+        }
+
+        public string ConnectionString
+        {
+            get
+            {
+                var string_builder = new SQLiteConnectionStringBuilder
+                {
+                    DataSource = DATABASE_NAME,
+                    Version = 3,
+                };
+
+                return string_builder.ToString();
+            }
+        }
+
+        /// <summary>
+        /// データベースファイルを作成する
+        /// </summary>
+        /// <param name="is_overwrite">同じ名称のデータベースファイルが存在する場合、上書きするか</param>
+        /// <returns>true = 作成した, false = 作成済み</returns>
+        public bool CreateDatabase(bool is_overwrite = false)
+        {
+            // 既にデータベースファイルを作成してある場合、テーブルやレコードが消えてしまう
+            if(!is_overwrite)
+            {
+                return false;
+            }
+
+            // データベースファイル名だけの場合、作成場所は Debug または Release ディレクトリ内になる
+            SQLiteConnection.CreateFile(ConnectionString);
+
+            return File.Exists(DATABASE_NAME);
+        }
+
+        /// <summary>
+        /// SQLを実行する
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <returns>命令を受けたレコード数</returns>
+        public int Execute(string sql)
+        {
+            if(string.IsNullOrWhiteSpace(sql))
+            {
+                return 0;
+            }
+
+            var result = 0;
+
+            using(var connection = new SQLiteConnection(ConnectionString))
+            {
+                connection.Open();
+
+                using(var transaction = connection.BeginTransaction())
+                {
+                    try
+                    {
+                        using(var command = connection.CreateCommand())
+                        {
+                            command.CommandText = sql;
+                            result = command.ExecuteNonQuery();
+
+                            transaction.Commit();
+                        }
+                    }
+                    catch(Exception exception)
+                    {
+                        transaction.Rollback();
+                        Debug.WriteLine(exception.Message);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ TrySQLiteConsole/UserRepository.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Diagnostics;
+
+namespace TrySQLiteConsole
+{
+    /// <summary>
+    /// User テーブルのみを扱うクラス
+    /// </summary>
+    public class UserRepository : RepositoryBase
+    {
+        public readonly string TABLE_NAME;
+
+        public UserRepository()
+        {
+            //TABLE_NAME = "Users";
+            TABLE_NAME = "User";
+        }
+
+        public void CreateTable()
+        {
+            var user = new User();
+
+            var sql = @$"
+                CREATE TABLE IF NOT EXISTS {TABLE_NAME}
+                (
+                    {nameof(user.Id)} INTEGER NOT NULL,
+                    {nameof(user.Name)} TEXT NOT NULL
+                );";
+
+            Execute(sql);
+        }
+
+        /// <summary>
+        /// テーブルに指定した User を挿入する
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns>true = 挿入成功, false = 挿入失敗</returns>
+        public bool Insert(User user)
+        {
+            if(user == null)
+            {
+                return false;
+            }
+
+            var sql = @$"
+                        INSERT INTO {TABLE_NAME} ({nameof(user.Id)}, {nameof(user.Name)})
+                        VALUES({user.Id}, '{user.Name}');
+                        ";
+
+            var result = Execute(sql);
+
+            return result > 0;
+        }
+
+        public void Update()
+        {
+
+        }
+
+        /// <summary>
+        /// テーブルにある User を参照する
+        /// </summary>
+        /// <returns></returns>
+        public List<User> Select()
+        {
+            var users = new List<User>();
+
+            try
+            {
+                using(var connection = new SQLiteConnection(ConnectionString))
+                {
+                    connection.Open();
+
+                    using(var command = connection.CreateCommand())
+                    {
+                        command.CommandText = $"SELECT * FROM {TABLE_NAME};";
+
+                        using(var reader = command.ExecuteReader())
+                        {
+                            while(reader.Read())
+                            {
+                                var user = new User
+                                {
+                                    Id = int.Parse(reader["Id"].ToString()),
+                                    Name = reader["Name"].ToString()
+                                };
+
+                                users.Add(user);
+                            }
+                        }
+                    }
+                }
+            }
+            catch(Exception exception)
+            {
+                Debug.WriteLine(exception.Message);
+            }
+
+            return users;
+        }
+
+        public void Delete()
+        {
+
+        }
+    }
+}
